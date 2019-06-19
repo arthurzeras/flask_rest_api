408abfabb5db5d204edaa3de0bb3b433db0be425
arthurzeras <arthurolvmorais@gmail.com>
2019-06-19 15:33:57 -0300

    Fazendo endpoint para salvar usuário

--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@
 
 # Importamos as classes API e Resource
 from flask_restful import Api, Resource
+from app.users.resources import SignUp
 
 # Criamos uma classe que extende de Resource
 class Index(Resource):
@@ -20,6 +21,9 @@ api = Api()
 def configure_api(app):
   # adicionamos na rota '/' a sua classe correspondente Index
   api.add_resource(Index, '/')
+
+  # rotas para o endpoint de usuarios
+  api.add_resource(SignUp, '/users')
 
   # inicializamos a api com as configurações do flask vinda por parâmetro
   api.init_app(app)
---
+++ app/users/utils.py
@@ -0,0 +1,12 @@
+def check_password_in_signup(password, confirm_password):
+
+  if not password:
+    return False
+
+  if not confirm_password:
+    return False
+
+  if not password == confirm_password:
+    return False
+
+  return True
