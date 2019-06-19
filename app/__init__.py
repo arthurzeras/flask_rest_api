d2014d05c2267d16345dfcad4f9754a028071fb0
arthurzeras <arthurolvmorais@gmail.com>
2019-06-19 13:40:34 -0300

    Configurando mongodb e criando model de usuario

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@
 from flask import Flask
 from config import config
 from .api import configure_api
+from .db import db
 
 def createApp(configName):
   app = Flask('api-users')
 
   app.config.from_object(config[configName])
+  
+  db.init_app(app)
+  
   configure_api(app)
 
   return app
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
   SECRET_KEY = getenv('SECRET_KEY') or 'uma string randômica e gigante'
   APP_PORT = int(getenv('APP_PORT'))
   DEBUG = eval(getenv('DEBUG').title())
+  MONGODB_HOST = getenv('MONGODB_URI')
 
 class DevelopmentConfig(Config):
   FLASK_ENV = 'development'
